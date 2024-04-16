3d02608b003b42feea5c6e3d81f61c0ee9d1be9c
Antony <antonygmurimi@gmail.com>
2024-04-16 13:46:14 +0300

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+const writeCards = (namesArr, eventName) =>{
+  const thankYouArray = [];
+  for (let i = 0; i < namesArr.length; i++){
+    thankYouArray.push(`Thank you, ${namesArr[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return thankYouArray;
+}
+
+
+function countDown(){
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+}
+countDown();
